39ae65502ccba736a889ef1b6cbca2b282a84689
pyneng <pyneng.course@gmail.com>
2019-02-01 18:48:17 +0000

    Загрузила решения в ветку task_check

--- exercises/04_data_structures/task_4_6.py
+++ exercises/04_data_structures/task_4_6.py
@@ -15,3 +15,22 @@ Outbound Interface     FastEthernet0/0
 '''
 
 ospf_route = 'O        10.0.24.0/24 [110/41] via 10.0.13.3, 3d18h, FastEthernet0/0'
+
+template = '''
+	Protocol:              {protocol}
+	Prefix:                {prefix}
+	AD/Metric:             {ad_metric}
+	Next-Hop:              {next_hop}
+	Last update:           {last_update}
+	Outbound Interface     {outbound_interface}
+	'''
+
+ospf_route = ospf_route.replace(',', '')
+ospf_route = ospf_route.replace('via ', '')
+ospf_route = ospf_route.replace('O', 'OSPF')
+#ospf_route = ospf_route.replace('[', '')
+#ospf_route = ospf_route.replace(']', '')
+
+protocol, prefix, ad_metric, next_hop, last_update, outbound_interface = ospf_route.split()
+ad_metric = ad_metric.strip('[]')
+print(template.format(protocol=protocol, prefix=prefix, ad_metric=ad_metric, next_hop=next_hop, last_update=last_update, outbound_interface=outbound_interface))
