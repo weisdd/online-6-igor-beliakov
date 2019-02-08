03c46d075a4ba517ec9c06f13273f8353b69b881
pyneng <pyneng.course@gmail.com>
2019-02-08 11:36:36 +0000

    Загрузила решения в ветку task_check

--- exercises/06_control_structures/task_6_2a.py
+++ exercises/06_control_structures/task_6_2a.py
@@ -14,3 +14,33 @@
 Ограничение: Все задания надо выполнять используя только пройденные темы.
 
 '''
+
+ip = input('Введите IP-адрес: ').strip()
+
+correct_octets_counter = 0
+
+octets = ip.split('.')
+
+if len(octets) == 4:
+	for octet in octets:
+		if octet.isdigit() and int(octet) in range(0,256):
+			correct_octets_counter += 1
+		else:
+			break
+
+	if correct_octets_counter == 4:
+		first_byte = int(octets[0])
+		if first_byte in range(1,128):
+			print('unicast')
+		elif first_byte in range(224,240):
+			print('multicast')
+		elif ip == '255.255.255.255':
+			print('local broadcast')
+		elif ip == '0.0.0.0':
+			print('unassigned')
+		else:
+			print('unused')
+	else:
+		print('Неправильный IP-адрес')
+else:
+	print('Неправильный IP-адрес')
