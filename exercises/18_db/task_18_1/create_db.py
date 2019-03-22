d7953b9b41bcbeee0e4bbc96fa67eb52fa334dcc
pyneng <pyneng.course@gmail.com>
2019-03-22 09:27:37 +0000

    Проверила задание 18.1

--- exercises/18_db/task_18_1/create_db.py
+++ exercises/18_db/task_18_1/create_db.py
@@ -20,3 +20,31 @@ def main():
 
 if __name__ == '__main__':
     main()
+
+# Все отлично
+
+# вариант решения
+
+import os
+import sqlite3
+
+
+# обрати внимание на параметры функции
+# это те файлы от которых она зависит
+def create_db(db_name, schema):
+    db_exists = os.path.exists(db_name)
+    if db_exists:
+        print('База данных существует')
+        return
+    print('Создаю базу данных...')
+    with open(schema, encoding='utf-8') as f:
+        schema_f = f.read()
+        connection = sqlite3.connect(db_name)
+        connection.executescript(schema_f)
+        connection.close()
+
+
+if __name__ == "__main__":
+    db_filename = 'dhcp_snooping.db'
+    schema_filename = 'dhcp_snooping_schema.sql'
+    create_db(db_filename, schema_filename)
